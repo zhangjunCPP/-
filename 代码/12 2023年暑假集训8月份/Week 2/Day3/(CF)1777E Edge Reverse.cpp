3ae4dda9273b27a397ee0be30d79c8a81d5ade9a
zhangjunCPP <136410188+zhangjunCPP@users.noreply.github.com>
2023-08-16 14:35:17 +0800

    2023.8.16

---
+++ 代码/12 2023年暑假集训8月份/Week 2/Day3/(CF)1777E Edge Reverse.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N=200005;
+vector<int> G[N];
+int dfn[N],low[N],idx;
+stack<int> s;
+int scc[N],tot;
+bool vis[N];
+void tarjan(int u){
+    dfn[u]=low[u]=++idx;
+    s.push(u);
+    vis[u]=true;
+    for(int v:G[u]){
+        if(!dfn[v]){
+            tarjan(v);
+            low[u]=min(low[u],low[v]);
+        }
+        else if(vis[v]){
+            low[u]=min(low[u],dfn[v]);
+        }
+    }
+    if(low[u]==dfn[u]){
+        ++tot;
+        while (s.top()!=u){
+            scc[s.top()]=tot;
+            vis[s.top()]=false;
+            s.pop();
+        }
+        scc[s.top()]=tot;
+        vis[s.top()]=false;
+        s.pop();
+    }
+}
+int ru[N];
+int u[N],v[N],w[N];
+int n,m;
+bool check(int mid){
+    for(int i=1;i<=n;i++) dfn[i]=low[i]=vis[i]=scc[i]=ru[i]=0;
+    while(!s.empty()) s.pop();
+    tot=idx=0;
+    for(int i=1;i<=n;i++) G[i].clear();
+
+    for(int i=1;i<=m;i++){
+        G[u[i]].push_back(v[i]);
+        if(w[i]>mid) continue;
+        G[v[i]].push_back(u[i]);
+    }
+    for(int i=1;i<=n;i++) if(!dfn[i]) tarjan(i);
+    for(int u=1;u<=n;u++){
+        for(int v:G[u]){
+            if(scc[u]!=scc[v]) ru[scc[v]]++;
+        }
+    }
+    int tmp=0;
+    for(int i=1;i<=tot;i++) if(ru[i]==0) tmp++;
+    return tmp==1;
+}
+void solve(){
+    cin>>n>>m;
+    int MAX=-1;
+    for(int i=1;i<=m;i++){
+        cin>>u[i]>>v[i]>>w[i];
+        MAX=max(MAX,w[i]);
+    }
+
+    int l=0,r=MAX+1;
+    while(l<r){
+        int mid=(l+r)>>1;
+        if(check(mid)) r=mid;
+        else l=mid+1;
+    }
+    if(l>MAX) cout<<"-1\n";
+    else cout<<l<<"\n";
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
---
+++ 代码/12 2023年暑假集训8月份/Week 2/Day3/(luogu)P4645 [COCI2006-2007#3] BICIKLI.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+const int mod=1e9;
+using namespace std;
+const int N=200005;
+
+vector<int> G[N];
+vector<int> G2[N];
+int in[N];
+
+int dfn[N],low[N],idx;
+stack<int> s;
+int scc[N],tot;
+bool vis[N];
+void tarjan(int u){
+    dfn[u]=low[u]=++idx;
+    s.push(u);
+    vis[u]=true;
+    for(int v:G[u]){
+        if(!dfn[v]){
+            tarjan(v);
+            low[u]=min(low[u],low[v]);
+        }
+        else if(vis[v]){
+            low[u]=min(low[u],dfn[v]);
+        }
+    }
+    if(low[u]==dfn[u]){
+        ++tot;
+        while (s.top()!=u){
+            scc[s.top()]=tot;
+            vis[s.top()]=false;
+            s.pop();
+        }
+        scc[s.top()]=tot;
+        vis[s.top()]=false;
+        s.pop();
+    }
+}
+
+bool vis1[N];
+void bfs1(){
+    queue<int> q;
+    q.push(1);
+    vis1[1]=true;
+    while (!q.empty()){
+        int now=q.front();
+        q.pop();
+        for(int v:G[now]){
+            if(!vis1[v]){
+                vis1[v]=true;
+                q.push(v);
+            }
+            in[v]++;
+        }
+    }
+}
+
+bool vis2[N];
+void bfs2(){
+    queue<int> q;
+    q.push(2);
+    vis2[2]=true;
+    while (!q.empty()){
+        int now=q.front();
+        q.pop();
+        for(int v:G2[now]){
+            if(!vis2[v]){
+                vis2[v]=true;
+                q.push(v);
+            }
+        }
+    }
+}
+
+int ans[N];
+void solve(){
+    queue<int> q;
+    q.push(1);
+    ans[1]=1;
+    while (!q.empty()){
+        int now=q.front();
+        q.pop();
+        for(int v:G[now]){
+            if(vis1[v]&&vis2[v]){
+                ans[v]+=ans[now];
+                ans[v]%=mod;
+                in[v]--;
+                if(in[v]==0) q.push(v);
+            }
+        }
+    }
+}
+
+int sum[N];
+int main(){
+    int n,m;
+    cin>>n>>m;
+    for(int i=1;i<=m;i++){
+        int u,v;
+        cin>>u>>v;
+        G[u].push_back(v);
+        G2[v].push_back(u);
+    }
+    for(int i=1;i<=n;i++) if(!dfn[i]) tarjan(i);
+    bfs1();
+    bfs2();
+    for(int i=1;i<=n;i++) sum[scc[i]]++;
+    for(int i=1;i<=n;i++) if(vis1[i]&&vis2[i]&&sum[scc[i]]>1) {puts("inf");return 0;}
+    solve();
+    cout<<ans[2];
+}

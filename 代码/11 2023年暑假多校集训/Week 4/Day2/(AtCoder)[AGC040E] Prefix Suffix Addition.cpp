79b845218bf66a7161d21bc611acca3f52262016
zhangjunCPP <136410188+zhangjunCPP@users.noreply.github.com>
2023-08-18 10:02:14 +0800

    2023.8.18

---
+++ 代码/11 2023年暑假多校集训/Week 4/Day2/(AtCoder)[AGC040E] Prefix Suffix Addition.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a[100000000];
+signed main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)cin>>a[i];
+    int ans=0;
+    int p=-1,q=-1;
+    for(int i=1;i<=n+1;i++){
+        int tmp=a[i]-a[i-1];
+        if(tmp>=0){
+            p=min(q,p+tmp);
+            q=q+tmp;
+        }
+        else{
+            p=max(min(q+tmp,p),-1);
+            if(q>=a[i]){
+                ans++;
+                q=p;
+                p=-1;
+            }
+        }
+    }
+    cout<<ans;
+}
---
+++ 代码/12 2023年暑假集训8月份/Week 2/Day5/(CF)1750F Majority.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+int dp[5005][5005];
+int s1[50005],s2[50005];
+int mod;
+int qpow(int x,int y){
+    int ans=1;
+    while(y){
+        if(y&1) ans=ans*x%mod;
+        x=x*x%mod;
+        y>>=1;
+    }
+    return ans%mod;
+}
+signed main(){
+    int n;
+    cin>>n>>mod;
+    dp[1][1]=1;
+    s1[2]=1;
+
+    for(int i=2;i<=n;i++){
+        for(int j=1;j<=n*2;j++)s2[j]=(s2[j-1]+s1[j])%mod;
+        dp[i][i]=qpow(2,i-2);
+        for(int j=1;j*2<i;j++){
+            dp[i][j]=(dp[j][j]*s2[i-j*2-1])%mod;
+            dp[i][i]=(dp[i][i]-dp[i][j]+mod)%mod;
+        }
+        for(int j=1;j<=i;j++)s1[i+j]=(s1[i+j]+dp[i][j])%mod;
+    }
+    cout<<dp[n][n]%mod;
+}

1f3da026f589adb56db2af45634a42e3651c27df
zhangjunCPP <136410188+zhangjunCPP@users.noreply.github.com>
2023-10-07 16:46:25 +0800

    2023.10.7

--- 代码/codeforces/1 2023.10.7 (VP)/(CF)1826D Running Miles.cpp
+++ 代码/codeforces/1 2023.10.7 (VP)/(CF)1826D Running Miles.cpp
@@ -3,12 +3,16 @@
 using namespace std;
 const int N=1e5+10;
 int a[N];
+int l[N],r[N];
 void solve(){
 	int n;
 	cin>>n;
+	for(int i=0;i<=n+1;i++) l[i]=r[i]=-0x3f3f3f3f;
 	for(int i=1;i<=n;i++) cin>>a[i];
+	for(int i=1;i<=n;i++) l[i]=max(l[i-1],a[i]+i);
+	for(int i=n;i>=1;i--) r[i]=max(r[i+1],a[i]-i);
 	int ans=0;
-	for(int l=1,r=n;l<=r;l++,r--) ans=__gcd(ans,abs(a[l]-a[r]));
+ 	for(int i=2;i<=n-1;i++) ans=max(ans,a[i]+l[i-1]+r[i+1]);
 	cout<<ans<<"\n";
 }
 signed main(){

c34a809a3039d922d9fb0a1792ed40f64f2e3f81
zhangjunCPP <136410188+zhangjunCPP@users.noreply.github.com>
2023-11-08 20:37:42 +0800

    2023.11.8

---
+++ 代码/3 初三上半学期/19 2023.11.8 期望概率与组合计数/(AtCoder)[AGC028B] Removing Blocks.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+const int mod=1e9+7;
+const int N=1e6+5;
+int inv[N];
+int s[N];
+int a[N];
+signed main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>a[i];
+    inv[1]=1;
+    for(int i=2;i<=n;i++) inv[i]=(mod-mod/i)*inv[mod%i]%mod;
+    for(int i=1;i<=n;i++) s[i]=(s[i-1]+inv[i])%mod;
+    int ans=0;
+    for(int i=1;i<=n;i++) ans=(ans+(s[i]+s[n-i+1]-1+mod)%mod*a[i]%mod)%mod;
+    for(int i=1;i<=n;i++) ans=ans*i%mod;
+    cout<<ans;
+    return 0;
+}

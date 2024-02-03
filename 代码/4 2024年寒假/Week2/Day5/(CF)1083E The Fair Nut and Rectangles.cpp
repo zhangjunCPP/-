93ab89e35901273bae7325f73a456bb2ecaf8c47
zhangjunCPP <136410188+zhangjunCPP@users.noreply.github.com>
2024-02-03 16:19:07 +0800

    2024.2.3

---
+++ 代码/4 2024年寒假/Week2/Day5/(CF)1083E The Fair Nut and Rectangles.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int N=5e6+10;
+struct node{int x,y,val;}a[N];
+bool cmp(const node&x,const node&y){return x.x<y.x;}
+int dp[N];
+int X(int i){return a[i].x;}
+int Y(int i){return dp[i];}
+double slope(int i,int j){
+    if(X(i)==X(j)) return Y(j)>=Y(i)?1e18:-1e18;
+    return (1.0*(Y(i)-Y(j)))/(1.0*(X(i)-X(j)));
+}
+int l,r,q[N];
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0),cout.tie(0);
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)cin>>a[i].x>>a[i].y>>a[i].val;
+    sort(a+1,a+n+1,cmp);
+    l=1,r=1;
+    q[1]=0;
+    int ans=0;
+    for(int i=1;i<=n;i++){
+        while(l<r&&slope(q[l],q[l+1])>=a[i].y)l++;
+        int j=q[l];
+        dp[i]=dp[j]+a[i].x*a[i].y-a[j].x*a[i].y-a[i].val;
+        ans=max(ans,dp[i]);
+        while(l<r&&slope(q[r-1],q[r])<=slope(q[r],i))r--;
+        q[++r]=i;
+    }
+    cout<<ans;
+
+    return 0;
+}
---
+++ 代码/4 2024年寒假/Week2/Day5/(luogu)P5017 [NOIP2018 普及组] 摆渡车.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int N=5e6+10;
+int sum1[N],sum2[N];
+int dp[N];
+signed main() {
+    int n,m;
+    cin>>n>>m;
+    int t=0;
+    for(int i=1;i<=n;i++){
+        int x;
+        cin>>x;
+        t=max(t,x);
+        sum1[x]++;
+        sum2[x]+=x;
+    }
+    for(int i=1;i<t+m;i++)sum1[i]+=sum1[i-1],sum2[i]+=sum2[i-1];
+    for(int i=1;i<t+m;i++){
+        dp[i]=sum1[i]*i-sum2[i];
+        for(int j=max(i-2*m+1,0ll);j<=i-m;j++)dp[i]=min(dp[i],dp[j]+i*(sum1[i]-sum1[j])-(sum2[i]-sum2[j]));
+    }
+    int ans=0x3f3f3f3f;
+    for(int i=t;i<t+m;i++)ans=min(ans,dp[i]);
+    cout<<ans;
+    return 0;
+}

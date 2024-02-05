4c47ee1c6ee27a5378d2aa83788b7bd1dce560b4
zhangjunCPP <136410188+zhangjunCPP@users.noreply.github.com>
2024-02-05 20:35:22 +0800

    2024.2.5

--- 代码/4 2024年寒假/Week2/Day6/(luogu)P7735 [NOI2021] 轻重边.cpp
+++ 代码/4 2024年寒假/Week2/Day6/(luogu)P7735 [NOI2021] 轻重边.cpp
@@ -2,20 +2,21 @@
 #define ls (k<<1)
 #define rs (k<<1|1)
 using namespace std;
-const int N=5e6+10;
-int n,m;
-int col[N];
-struct node{int v,nex;}a[N];
+const int N=1e6+10;
+
+struct node{
+    int v,nex;
+} a[N];
 int first[N],cnt;
 void add(int u,int v){
     a[++cnt]={v,first[u]};
     first[u]=cnt;
-    return;
 }
+
 int dep[N],siz[N],son[N],fa[N];
 void dfs1(int u,int dad){
-    fa[u]=dad;
     siz[u]=1;
+    fa[u]=dad;
     dep[u]=dep[dad]+1;
     for(int i=first[u];i;i=a[i].nex){
         int v=a[i].v;
@@ -25,102 +26,102 @@ void dfs1(int u,int dad){
         if(siz[v]>siz[son[u]])son[u]=v;
     }
 }
-int top[N],id[N],rev[N],tot;
+
+int id[N],tot;
+int top[N];
 void dfs2(int u){
     if(son[u]){
         int v=son[u];
         top[v]=top[u];
         id[v]=++tot;
-        rev[id[v]]=v;
-        dfs2(v);
+        dfs2(son[u]);
     }
     for(int i=first[u];i;i=a[i].nex){
         int v=a[i].v;
         if(top[v])continue;
         top[v]=v;
         id[v]=++tot;
-        rev[id[v]]=v;
         dfs2(v);
     }
-    return;
 }
 
-
 struct node2{
-    int sum;
-    int lc,rc;
-    int lazy;
-    node2 operator+(const node2&oth){
+    int sum=0;
+    int lc,rc=0;
+    int lazy=0;
+    node2 operator+(const node2 &oth){
         if(!lc)return oth;
         if(!oth.lc)return *this;
         node2 ans;
-        ans.sum=sum+oth.sum-(rc==oth.lc);
+        ans.sum=sum+oth.sum+(rc==oth.lc);
         ans.lc=lc;
         ans.rc=oth.rc;
         ans.lazy=0;
         return ans;
     }
 }tree[N*4];
-
-void pushup(int k){
-    tree[k].sum=tree[ls].sum+tree[rs].sum-(tree[ls].rc==tree[rs].lc);
-    if(tree[ls].sum)tree[k].lc=tree[ls].lc;else tree[k].lc=tree[rs].lc;
-    if(tree[rs].sum)tree[k].rc=tree[rs].rc;else tree[k].rc=tree[ls].rc;
-    return;
+void update(int k,int l,int r,int c){
+    tree[k].sum=r-l;
+    tree[k].lc=tree[k].rc=c;
+    tree[k].lazy=c;
 }
-void update(int k,int val){
-    tree[k].sum=1;
-    tree[k].lc=tree[k].rc=val;
-    tree[k].lazy=val;
-    return;
-}
-void pushdown(int k){
+void pushdown(int k,int l,int r){
     if(tree[k].lazy){
-        update(ls,tree[k].lazy);
-        update(rs,tree[k].lazy);
+        int mid=(l+r)>>1;
+        update(ls,l,mid,tree[k].lazy);
+        update(rs,mid+1,r,tree[k].lazy);
         tree[k].lazy=0;
     }
 }
 void build(int k,int l,int r){
     if(l==r){
-        tree[k]={1,col[rev[l]],col[rev[l]],0};
+        tree[k]={0,l,l,0};
         return;
     }
     int mid=(l+r)>>1;
     build(ls,l,mid);
     build(rs,mid+1,r);
-    pushup(k);
-    return;
+    tree[k]=tree[ls]+tree[rs];
 }
 void change(int k,int l,int r,int x,int y,int val){
     if(l>y||r<x)return;
-    if(x<=l&&r<=y){update(k,val);return;}
+    if(x<=l&&r<=y){
+        update(k,l,r,val);
+        return;
+    }
     int mid=(l+r)>>1;
-    pushdown(k);
+    pushdown(k,l,r);
     change(ls,l,mid,x,y,val);
     change(rs,mid+1,r,x,y,val);
-    pushup(k);
-    return;
+    tree[k]=tree[ls]+tree[rs];
 }
 node2 query(int k,int l,int r,int x,int y){
     if(x<=l&&r<=y)return tree[k];
     int mid=(l+r)>>1;
-    pushdown(k);
+    pushdown(k,l,r);
     if(y<=mid)return query(ls,l,mid,x,y);
     if(x>mid)return query(rs,mid+1,r,x,y);
     return query(ls,l,mid,x,mid)+query(rs,mid+1,r,mid+1,y);
 }
-void modify(int x,int y,int val){
+void init(){
+    memset(first,0,sizeof first);
+    memset(son,0,sizeof son);
+    memset(top,0,sizeof top);
+    cnt=0;
+    tot=0;
+}
+int now;
+int n,m;
+void modify(int x,int y){
     int fx=top[x],fy=top[y];
     while(fx!=fy){
         if(dep[fx]<dep[fy]){swap(fx,fy);swap(x,y);}
-        change(1,1,n,id[fx],id[x],val);
+        change(1,1,n,id[fx],id[x],now);
         x=fa[fx];
         fx=top[x];
     }
     if(dep[x]>dep[y])swap(x,y);
-    change(1,1,n,id[x],id[y],val);
-    return;
+    change(1,1,n,id[x],id[y],now);
 }
 int ask(int x,int y){
     node2 ans1,ans2;
@@ -144,9 +145,10 @@ int ask(int x,int y){
     swap(ans1.lc,ans1.rc);
     return (ans1+ans2).sum;
 }
-int main(){
+void solve(){
+    init();
     cin>>n>>m;
-    for(int i=1;i<=n;i++)cin>>col[i];
+    now=n;
     for(int i=1;i<n;i++){
         int u,v;
         cin>>u>>v;
@@ -156,22 +158,23 @@ int main(){
     dfs1(1,0);
     top[1]=1;
     id[1]=++tot;
-    rev[id[1]]=1;
     dfs2(1);
     build(1,1,n);
     while(m--){
-        string opt;
-        cin>>opt;
-        if(opt=="C"){
-            int u,v,w;
-            cin>>u>>v>>w;
-            modify(u,v,w);
+        int opt,u,v;
+        cin>>opt>>u>>v;
+        if(opt==1) {
+            now++;
+            modify(u,v);
         }
-        if(opt=="Q"){
-            int u,v;
-            cin>>u>>v;
-            cout<<ask(u,v)<<"\n";
-        }
+        if(opt==2)cout<<ask(u,v)<<"\n";
     }
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0),cout.tie(0);
+    int T;
+    cin>>T;
+    while(T--)solve();
     return 0;
 }

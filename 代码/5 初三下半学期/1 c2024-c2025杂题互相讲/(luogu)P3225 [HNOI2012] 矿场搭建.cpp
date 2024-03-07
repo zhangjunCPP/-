b78afe81435cb6c1cf28ea2cc73a20f6c476c55c
zhangjunCPP <136410188+zhangjunCPP@users.noreply.github.com>
2024-03-07 21:15:14 +0800

    2024.3.7

---
+++ 代码/5 初三下半学期/1 c2024-c2025杂题互相讲/(luogu)P3225 [HNOI2012] 矿场搭建.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int N=1010;
+struct node{
+    int v,nex;
+}a[N];
+int first[N],cnt=1;
+void add(int u,int v){
+    a[++cnt]={v,first[u]};
+    first[u]=cnt;
+}
+int dfn[N],low[N],tot;
+bool cut[N];
+stack<int>s;
+int col;
+vector<int> ans[N];
+int rt;
+void Tarjan(int u,int edge){
+    dfn[u]=low[u]=++tot;
+    s.push(u);
+    int sum=0;
+    for(int i=first[u];i;i=a[i].nex){
+        sum++;
+        int v=a[i].v;
+        if(!dfn[v]){
+            Tarjan(v,i);
+            low[u]=min(low[u],low[v]);
+            if((u==rt&&sum>1)||(u!=rt&&low[v]>=dfn[u]))cut[u]=true;
+            if(low[v]>=dfn[u]){
+                ++col;
+                ans[col].push_back(u);
+                while(s.top()!=v){
+                    ans[col].push_back(s.top());
+                    s.pop();
+                }
+                ans[col].push_back(v);
+                s.pop();
+            }
+        }
+        else if(i!=(edge^1))low[u]=min(low[u],dfn[v]);
+    }
+}
+int u[N],v[N];
+void init(){
+    cnt=1,tot=col=0;
+    memset(first,0,sizeof first);
+    memset(low,0,sizeof low);
+    memset(dfn,0,sizeof dfn);
+    memset(cut,0,sizeof cut);
+    for(int i=0;i<N;i++)ans[i].clear();
+}
+signed main(){
+    int Case=0;
+    int n,m;
+    cin>>m;
+    while(true){
+        init();
+        Case++;
+        n=0;
+        if(m==0)return 0;
+        for(int i=1;i<=m;i++){
+            cin>>u[i]>>v[i];
+            n=max({n,u[i],v[i]});
+            add(u[i],v[i]);
+            add(v[i],u[i]);
+        }
+        for(int i=1;i<=n;i++)if(!dfn[i])rt=i,Tarjan(i,0);
+        if(col==1)printf("Case %lld: 2 %lld\n",Case,n*(n-1)/2);
+        else{
+            int ans1=0,ans2=1;
+            for(int i=1;i<=col;i++){
+                int sum=0;
+                for(int j:ans[i])if(cut[j])sum++;
+                if(sum==1)ans1++,ans2*=ans[i].size()-1;
+            }
+            printf("Case %lld: %lld %lld\n",Case,ans1,ans2);
+        }
+        cin>>m;
+    }
+}
---
+++ 代码/5 初三下半学期/1 c2024-c2025杂题互相讲/(luogu)P4768 [NOI2018] 归程.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N=5e6+10;
+int n,m;
+struct node{int v,w,nex;}a[N];
+int first[N],cnt;
+void add(int u,int v,int w){
+    a[++cnt]={v,w,first[u]};
+    first[u]=cnt;
+}
+struct node2{int u,v,l,a;}G[N];
+struct node3{
+    int id,val;
+    bool operator<(const node3&oth)const{return val>oth.val;}
+};
+int dis[N];
+bool vis[N];
+void dij(){
+    priority_queue<node3>q;
+    memset(dis,0x3f,sizeof dis);
+    memset(vis,false,sizeof vis);
+    dis[1]=0;
+    q.push({1,0});
+    while(!q.empty()){
+        int u=q.top().id;
+        q.pop();
+        if(vis[u])continue;
+        vis[u]=true;
+        for(int i=first[u];i;i=a[i].nex){
+            int v=a[i].v;
+            if(dis[v]>dis[u]+a[i].w){
+                dis[v]=dis[u]+a[i].w;
+                q.push({v,dis[v]});
+            }
+        }
+    }
+}
+int fa[N];
+int find(int x){if(fa[x]!=x)fa[x]=find(fa[x]);return fa[x];}
+bool cmp(const node2&x,const node2&y){return x.a>y.a;}
+int val[N];
+int Min[N];
+int fa2[N][20];
+void dfs(int u,int dad){
+    fa2[u][0]=dad;
+    for(int i=1;i<20;i++)fa2[u][i]=fa2[fa2[u][i-1]][i-1];
+    if(u<=n)Min[u]=dis[u];
+    for(int i=first[u];i;i=a[i].nex){
+        int v=a[i].v;
+        dfs(v,u);
+        Min[u]=min(Min[u],Min[v]);
+    }
+}
+int calc(int u,int p){
+    for(int i=19;i>=0;i--)if(val[fa2[u][i]]>p)u=fa2[u][i];
+    return u;
+}
+void init(){
+    memset(Min,0x3f,sizeof Min);
+    memset(first,0,sizeof first);
+    cnt=0;
+}
+void solve(){
+    init();
+    cin>>n>>m;
+    for(int i=1;i<=m;i++){
+        cin>>G[i].u>>G[i].v>>G[i].l>>G[i].a;
+        add(G[i].u,G[i].v,G[i].l);
+        add(G[i].v,G[i].u,G[i].l);
+    }
+    dij();
+    memset(first,0,sizeof first);
+    cnt=0;
+    sort(G+1,G+m+1,cmp);
+    for(int i=1;i<n*2;i++)fa[i]=i;
+    int tot=n;
+    for(int i=1;i<=m;i++){
+        int fu=find(G[i].u),fv=find(G[i].v);
+        if(fu!=fv){
+            fa[fu]=fa[fv]=++tot;
+            val[tot]=G[i].a;
+            add(tot,fu,0),add(tot,fv,0);
+            if(tot==n*2-1)break;
+        }
+    }
+    dfs(tot,0);
+    int q,K,S;
+    cin>>q>>K>>S;
+    int lastans=0;
+    while(q--){
+        int v,p;
+        cin>>v>>p;
+        v=(v+K*lastans-1)%n+1;
+        p=(p+K*lastans)%(S+1);
+        lastans=Min[calc(v,p)];
+        cout<<lastans<<"\n";
+    }
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr),cout.tie(nullptr);
+    int T;
+    cin>>T;
+    while(T--)solve();
+}

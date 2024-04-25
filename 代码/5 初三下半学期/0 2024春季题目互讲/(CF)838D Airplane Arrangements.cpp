7d19c74e2f8854c185756638e8c5614d4f2ed2ac
zhangjunCPP <136410188+zhangjunCPP@users.noreply.github.com>
2024-04-25 17:02:05 +0800

    2024.4.25

---
+++ 代码/5 初三下半学期/0 2024春季题目互讲/(CF)838D Airplane Arrangements.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+const int mod=1e9+7;
+int qpow(int x,int y){
+    int ans=1;
+    while(y){
+        if(y&1)ans=ans*x%mod;
+        y>>=1;
+        x=x*x%mod;
+    }
+    return ans%mod;
+}
+signed main(){
+    int n,m;
+    cin>>n>>m;
+    int ans=qpow(2*n+2,m);
+    ans=ans*(n+1-m+mod)%mod*qpow(n+1,mod-2)%mod;
+    cout<<ans;
+}
